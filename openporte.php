f6a6eba2eedce1ea1ecdd85a9c679e1194ca8e91
Jean-Christophe Berthon <brunnen.brezel_83@icloud.com>
2026-06-19 16:37:01 +0200

    i18n: Added translations for DE, ES, FR, IT, NL and PT

--- openporte.php
+++ openporte.php
@@ -15,6 +15,7 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * License: GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: openporte
+ * Domain Path: /languages
  */
 
 /*
